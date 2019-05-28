98255de3d847f99b0b2787bd050cf2b563eddd2e
Rolandas Razma <rolandas@razma.lt>
2019-05-28 16:02:48 +0100

    use queue in InMemoryNormalizedCache to avoid race conditions

--- Sources/Apollo/InMemoryNormalizedCache.swift
+++ Sources/Apollo/InMemoryNormalizedCache.swift
@@ -1,21 +1,37 @@
+import Dispatch
+
 public final class InMemoryNormalizedCache: NormalizedCache {
   private var records: RecordSet
-
+  private let queue: DispatchQueue
+  
   public init(records: RecordSet = RecordSet()) {
     self.records = records
+    self.queue = DispatchQueue(label: "com.apollographql.InMemoryNormalizedCache", attributes: .concurrent)
   }
-
+  
   public func loadRecords(forKeys keys: [CacheKey]) -> Promise<[Record?]> {
-    let records = keys.map { self.records[$0] }
-    return Promise(fulfilled: records)
+    return Promise<[Record?]> { fulfill, reject in
+      queue.async(flags: .barrier) {
+        let records = keys.map { self.records[$0] }
+        fulfill(records)
+      }
+    }
   }
-
+  
   public func merge(records: RecordSet) -> Promise<Set<CacheKey>> {
-    return Promise(fulfilled: self.records.merge(records: records))
+    return Promise<Set<CacheKey>> { fulfill, reject in
+      queue.async(flags: .barrier) {
+        fulfill(self.records.merge(records: records))
+      }
+    }
   }
-
+  
   public func clear() -> Promise<Void> {
-    records.clear()
-    return Promise(fulfilled: ())
+    return Promise<Void> { fulfill, reject in
+      queue.async(flags: .barrier) {
+        self.records.clear()
+        fulfill(())
+      }
+    }
   }
 }
